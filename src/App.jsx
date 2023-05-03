c4e68d33204f3750180983b7660bb4e33d495229
njn-R <njn@protonmail.com>
2023-05-03 13:06:45 +0530

    useContext Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
+import { createContext } from 'react'
 import './App.css'
 import Tours from './components/Tours'
 import Loading from './components/Loading'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+
+export const tourContext = createContext()
 
 function App() {
   const [isLoading, setIsLoading] = useState(true)
   const [tours, setTours] = useState([])
-  const url = "http://localhost:3000/tours";
+  const url = 'http://localhost:3000/tours'
 
   const fetchUsers = async () => {
     try {
-      const response = await fetch(url);
-      const tours = await response.json();
+      const response = await fetch(url)
+      const tours = await response.json()
       setTours(tours)
-      setIsLoading(false)  
+      setIsLoading(false)
     } catch (error) {
-      setIsLoading(false);  
+      setIsLoading(false)
       console.log(error)
     }
-    
   }
 
-  useEffect( () => { 
+  useEffect(() => {
     fetchUsers()
-  },[])
+  }, [])
 
   return (
-    <main>
-      {isLoading ? <Loading /> : <Tours tours= {tours} />}
-    </main>
+    <tourContext.Provider value={ tours }>
+      <main>{isLoading ? <Loading /> : <Tours/>}</main>
+    </tourContext.Provider>
   )
 }
 
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,56 +1,57 @@
-import React, {useEffect, useState} from "react";
-import Tour from "./Tour";
+import React, { useEffect, useState, useContext } from 'react'
+import Tour from './Tour'
+import { tourContext } from '../App'
 
-const Tours = ({ tours }) => {
-    const [tourList, setTourList] = useState(tours);
-    const [isEmpty, setIsEmpty] = useState(false);
+const Tours = () => {
+  const tours = useContext(tourContext)
+  const [tourList, setTourList] = useState(tours)
+  const [isEmpty, setIsEmpty] = useState(false)
 
-    const handleDelete = (id) => {
-       const newTourList = tourList.filter((data) => data.id !== id);
-       setTourList(newTourList);
-    };
+  const handleDelete = (id) => {
+    const newTourList = tourList.filter((data) => data.id !== id)
+    setTourList(newTourList)
+  }
 
-    const resetTours = () => {
-        setTourList(tours);
-        setIsEmpty(false);
-    };
+  const resetTours = () => {
+    setTourList(tours)
+    setIsEmpty(false)
+  }
 
-    useEffect(() => {
-      if (tourList.length === 0) {
-        setIsEmpty(true);
-      }
-    }, [tourList]);
+  useEffect(() => {
+    if (tourList.length === 0) {
+      setIsEmpty(true)
+    }
+  }, [tourList])
 
-    const ResetButton = () => {
-      return (
-        isEmpty && (
-          <React.Fragment>
-            <h4>There are currently no tours available.</h4>
-            <button className='btn btn-secondary' onClick={() => resetTours()}>
-              Reset Tours
-            </button>
-          </React.Fragment>
-        )
+  const ResetButton = () => {
+    return (
+      isEmpty && (
+        <React.Fragment>
+          <h4>There are currently no tours available.</h4>
+          <button className='btn btn-secondary' onClick={() => resetTours()}>
+            Reset Tours
+          </button>
+        </React.Fragment>
       )
-    };
-    
-    
+    )
+  }
+
   return (
     <>
-      <div className="title">
+      <div className='title'>
         <h1>Our Tours</h1>
-        <div className="underline"></div>
+        <div className='underline'></div>
       </div>
       <div>
         {tourList.map((tour) => {
           return (
             <Tour key={tour.id} tourList={tour} handleDelete={handleDelete} />
-          );
+          )
         })}
         <ResetButton />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Tours;
+export default Tours
